1936d97562d2ebc73b612b60888583837d41198c
Vinuthnasadu <117430692+Vinuthnasadu@users.noreply.github.com>
2023-01-22 22:28:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
 //your code here
+   const userImg = document.getElementById("user-img");
+    const userName = document.getElementById("user-name");
+    const additionalInfo = document.getElementById("additional-info");
+    const ageBtn = document.getElementById("age-btn");
+    const emailBtn = document.getElementById("email-btn");
+    const phoneBtn = document.getElementById("phone-btn");
+    const getUserBtn = document.getElementById("getUser-btn");
+
+    let userData;
+
+    // Fetch user data on page load
+    fetch("https://randomuser.me/api/")
+      .then(response => response.json())
+      .then(data => {
+        userData = data.results[0];
+        userImg.src = userData.picture.large;
+        userName.innerText = `${userData.name.first} ${userData.name.last}`;
+      });
+
+    // Handle button clicks
+    ageBtn.addEventListener("click", () => {
+      additionalInfo.innerText = `Age: ${userData.dob.age}`;
+    });
+    emailBtn.addEventListener("click", () => {
+      additionalInfo.innerText = `Email: ${userData.email}`;
+    });
+    phoneBtn.addEventListener("click", () => {
+      additionalInfo.innerText = `Phone: ${userData.phone}`;
+    });
+    getUserBtn.addEventListener("click", () => {
+      fetch("https://randomuser.me/api/")
+        .then(response => response.json())
+        .then(data => {
+          userData = data.results[0];
+          userImg.src = userData.picture.large;
+          userName.innerText = `${userData.name.first} ${userData.name.last}`;
+          additionalInfo.innerText = "";
+        });
+    });
